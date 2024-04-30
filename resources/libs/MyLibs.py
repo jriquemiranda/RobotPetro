4052261156a9935cc115bc9d2c85b5ecf753c583
Henrique Miranda <jriquemiranda@gmail.com>
2024-04-30 09:12:36 -0300

    Testes de acesso ao banco

--- resources/libs/MyLibs.py
+++ resources/libs/MyLibs.py
@@ -1,2 +1,21 @@
-def my_log(text):
-    print(text)
+import psycopg2
+
+def consultar_passagem(ACESSO,CODIGO):
+    
+    conn = psycopg2.connect(
+        host=ACESSO.HOST,
+        database=ACESSO.DATABASE,
+        user=ACESSO.USER,
+        password=ACESSO.PASSWORD
+    )
+
+    query = f"SELECT * FROM bdo_servico bs WHERE codigo = '{CODIGO}';"
+
+    cur = conn.cursor()
+    cur.execute(query)    
+    resp = cur.fetchall()
+
+    conn.commit()
+    conn.close()
+    
+    return resp
